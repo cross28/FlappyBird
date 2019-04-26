ffc3bb590600408d45fb943e8b218e200e33da35
cross28 <icross@lc.edu>
2019-04-26 00:00:45 -0500

    Made the button class. Would allow the button to set its position, width, and height. Also changes colors depending on whether the user is hovering over it, pressing it, or is just sitting statically

--- include/Button.h
+++ include/Button.h
@@ -1,16 +1,33 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include <SFML/Graphics.hpp>
+#include <string>
+
+enum button_states{btn_idle = 0, btn_hover, btn_active};
 
 class Button
 {
-    public:
-        Button();
-        virtual ~Button();
+private:
+    short unsigned buttonState;
 
-    protected:
+    sf::RectangleShape shape;
+    sf::Font* font;
+    sf::Text text;
 
-    private:
+    sf::Color idleColor;
+    sf::Color hoverColor;
+    sf::Color activeColor;
+
+public:
+    Button(float x, float y, float width, float height, sf::Font* font,
+            std::string text, sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
+    virtual ~Button();
+
+    const bool isPressed() const;
+
+    void draw(sf::RenderWindow& window);
+    void update(const sf::Vector2f mousePos);
 };
 
 #endif // BUTTON_H
